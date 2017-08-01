e990642b7fc044054e3c1c3a7020d22d6e34e206
YessiGarcia <aria.801900@gmail.com>
2017-07-31 23:58:12 -0500

    helper en el nav

---
+++ dist/handlebars.js
@@ -0,0 +1,14 @@
+var template = Handlebars.templates["index"];
+
+var data ={};
+
+Handlebars.registerHelper("linkto", function(url, name){
+	var name = Handlebars.Utils.escapeExpression(name),
+		url = Handlebars.Utils.escapeExpression(url),
+		link = "<a href=\'" + location.protocol + "//" + location.hots + "/" + url + ".html\'>" + name + "</a>"
+
+	return new Handlebars.SafeString(link);
+})
+
+//var compiladorHTML = compilar(data);
+document.getElementById("nav-mobile").innerHTML += template(data);
