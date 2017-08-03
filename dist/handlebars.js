22113e2cf78280f4f3b2ffe3e5da26b5b1cded44
YessiGarcia <aria.801900@gmail.com>
2017-08-03 15:07:13 -0500

    home casi completo

--- dist/handlebars.js
+++ dist/handlebars.js
@@ -1,6 +1,10 @@
 var template = Handlebars.templates["index"];
 
+//var template2 = Handlebars.templates["index2"];
+
 var data ={};
+
+//var data2={};
 
 Handlebars.registerHelper("linkto", function(url, name){
 	var name = Handlebars.Utils.escapeExpression(name),
@@ -10,5 +14,15 @@ Handlebars.registerHelper("linkto", function(url, name){
 	return new Handlebars.SafeString(link);
 })
 
+/*Handlebars.registerHelper("producto", function(nombre, precio){
+	var name = Handlebars.Utils.escapeExpression(nombre),
+	var price = Handlebars.Utils.escapeExpression(precio),
+	var parrafo = "<p>" + name + price + "</p>"
+
+	return new Handlebars.SafeString(parrafo);
+})*/
+
 //var compiladorHTML = compilar(data);
 document.getElementById("nav-mobile").innerHTML += template(data);
+
+//document.getElementById("tarjeta").innerHTML += template2(data2);
